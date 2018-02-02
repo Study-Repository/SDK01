dc0690a0df0e62e739d0311fafba1dd838efa802
谢强 <xieqiang@kws-info.com>
2018-02-02 14:30:48 +0800

    #初始化项目
    swift项目

---
+++ SDK_Swift/SDK_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDK_Swift
+//
+//  Created by 谢强 on 2018/2/2.
+//  Copyright © 2018年 谢强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
